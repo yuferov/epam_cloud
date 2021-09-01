52db915c0ffd5588672eb171ca341f86f9cc50e9
Konstantin Yuferov <yuferov@pm.me>
2021-09-02 01:07:44 +0300

    My solution for python's second homework.

--- python/python-homework-2.py
+++ python/python-homework-2.py
@@ -1,12 +1,7 @@
 import random
 import statistics
 
-# If you read that comment, here's an additional link for you: https://pyformat.info/
-
-
 class Metrics:
-    """Don't worry about classes for now. We just need the data"""
-
     def __init__(self):
         self.cpu_data = [random.randint(0, 100) for _ in range(8)]
         self.memory_used = random.randint(300, 2048)
@@ -17,88 +12,28 @@ class Metrics:
         self.agent = "metric-gatherer.%s" % random.randint(1000, 9999)
         self.agent_address = random.choice([16384, 32768, 4096])
 
+M = Metrics()
 
-def format_with_fstring(data: Metrics):
-    """Return a string that is formatted with `data` by using an "f-string"
-    that would have the following format:
-      CPU #1: 10%, Memory used: 450, Load avg: 3.23
+def format_with_fstring():
+    sys_info = f"CPU #1: {M.cpu_data[1]}%, Memory used: {M.memory_used}, Load avg: {M.load_avg:.2f}"
+    return f"{sys_info:*^64}"
 
-    Requirements:
-        * The string should limit all floats to two digits after decimal (e.g. 3.1415 -> 3.14)
-        * The text format should be exactly as in comment (tests would check for that)
-        * The string should be centered and padded with asterisks ("*") to have a total length of 64.
-    For example:
-        "CPU #1: 10%, Memory used: 450, Load avg: 3.23"
-    should become
-        "*********CPU #1: 10%, Memory used: 450, Load avg: 3.23**********"
+def format_with_format():
+    format_pid = M.agent 
+    sys_info = "[{pid}] CPU #7: {cpu7}%, Memory used: {mused}, Load avg: {lavg:.2f}".format(
+            pid = M.agent[-4:],
+            cpu7 = M.cpu_data[7],
+            mused = M.memory_used,
+            lavg = M.load_avg
+            )
+    return "{:->64}".format(sys_info)
 
-    Args:
-        data (Metrics): Data you have to format
-
-    Links:
-        https://docs.python.org/3/reference/lexical_analysis.html#formatted-string-literals
-    """
-    # ... write the code...
-    # note: you might have an temporary string here, that's fine!
-
-
-def format_with_format(data: Metrics):
-    """Return a string that is formatted with `data` by using a "format" method
-    that would have the following format:
-      [3124] CPU #7: 25%, Memory used: 450, Load avg: 3.23
-    where:
-        3124 -- is a pid of gatherer agent from metrics data (the digits after "metric-gatherer." in data.agent,
-        for example if data.agent is "metric-gatherer.3124")
-        25 - is the stat for 5th CPU in data
-        450 - is a used memory metric from data
-        3.23 - is a load_avg from data limited to two digits after decimal (3.2345 -> 3.23)
-
-    Requirements:
-        * The string should limit all floats to two digits after decimal (e.g. 3.1415 -> 3.14)
-        * The text format should be exactly as in comment (tests would check for that)
-        * The string should be right aligned and padded with dashes ("-") to have a total length of 64.
-    For example:
-        "[3124] CPU #7: 25%, Memory used: 450, Load avg: 3.23"
-    should become
-        "------------[3124] CPU #7: 25%, Memory used: 450, Load avg: 3.23"
-
-    Args:
-        data (Metrics): Data you have to format
-
-    Links:
-        https://docs.python.org/3/library/string.html#format-examples
-    """
-    # ... write the code...
-    # temporary string is fine there too
-
-
-def format_with_percent(data: Metrics):
-    """Return a string that is formatted with `data` by using a "%" formatting
-    that would have the following format:
-      [0x4000] CPU #7: 87%, Memory used: 900, Load avg: 1.25
-    where:
-        0x4000 -- is an adress of agent in memory from data.agent_address converted to hexadecimal
-        87 - is the stat for 7th CPU in data
-        900 - is a used memory metric from data
-        1.25 - is a load_avg from data limited to two digits after decimal (1.2489 -> 1.25)
-
-    Requirements:
-        * The string should limit all floats to two digits after decimal (e.g. 3.1415 -> 3.14)
-        * The text format should be exactly as in comment (tests would check for that)
-        * The string should be left aligned and padded with spaces (" ") to have a total length of 64.
-    For example:
-        "[0x4000] CPU #7: 87%, Memory used: 900, Load avg: 1.25"
-    should become
-        "'          [0x4000] CPU #7: 87%, Memory used: 900, Load avg: 1.25'"
-
-    Args:
-        data (Metrics): Data you have to format
-
-    Links:
-        https://docs.python.org/3/library/stdtypes.html#printf-style-string-formatting
-    """
-    # ... write the code...
-    # temporary string is fine here too
+def format_with_percent():
+    pid = M.agent_address
+    cpu_7 = M.cpu_data
+    mused = M.memory_used
+    lavg = M.load_avg
+    return "%*s" % (64, "[%#x] CPU #7: %i%%, Memory used: %i, Load avg: %.2f" % (M.agent_address, M.cpu_data[7], M.memory_used, M.load_avg))
 
 
 #### Tests ####
@@ -109,7 +44,7 @@ from string import Template
 
 class TestStringFormatters(unittest.TestCase):
     def get_data(self):
-        return Metrics()
+        return M
 
     def get_pid(self, agent: str) -> str:
         acc = ""
@@ -123,24 +58,18 @@ class TestStringFormatters(unittest.TestCase):
         return acc
 
     def test_fstring(self):
-        """
-        For example:
-            "CPU #1: 10%, Memory used: 450, Load avg: 3.23"
-        should become
-            "*********CPU #1: 10%, Memory used: 450, Load avg: 3.23**********"
-        """
         data = self.get_data()
         fstring_template = Template(
             "CPU #1: $cpu_1%, Memory used: $mem_used, Load avg: $load_avg"
         )
         templated = fstring_template.substitute(
-            cpu_1=data.cpu_data[1],
-            mem_used=data.memory_used,
-            load_avg=round(data.load_avg, 2),
+            cpu_1 = data.cpu_data[1],
+            mem_used = data.memory_used,
+            load_avg = f"{data.load_avg:.2f}"
         )
         formatted = templated.center(64, "*")
 
-        user_result = format_with_fstring(data)
+        user_result = format_with_fstring()
         self.assertEqual(user_result, formatted)
 
     def test_format_method(self):
@@ -155,16 +84,16 @@ class TestStringFormatters(unittest.TestCase):
             "[$pid] CPU #7: $cpu_7%, Memory used: $mem_used, Load avg: $load_avg"
         )
         templated = fstring_template.substitute(
-            pid=self.get_pid(data.agent),
-            cpu_7=data.cpu_data[7],
-            mem_used=data.memory_used,
-            load_avg=round(data.load_avg, 2),
+            pid = self.get_pid(data.agent),
+            cpu_7 = data.cpu_data[7],
+            mem_used = data.memory_used,
+            load_avg = '{:.2f}'.format(data.load_avg)
         )
         formatted = templated.rjust(64, "-")
 
-        user_result = format_with_format(data)
+        user_result = format_with_format()
         self.assertEqual(user_result, formatted)
-
+    
     def test_format_percent(self):
         """
         For example:
@@ -177,14 +106,14 @@ class TestStringFormatters(unittest.TestCase):
             "[$hex] CPU #7: $cpu_7%, Memory used: $mem_used, Load avg: $load_avg"
         )
         templated = fstring_template.substitute(
-            hex=hex(data.agent_address),
-            cpu_7=data.cpu_data[7],
-            mem_used=data.memory_used,
-            load_avg=round(data.load_avg, 2),
+            hex = hex(data.agent_address),
+            cpu_7 = data.cpu_data[7],
+            mem_used = data.memory_used,
+            load_avg = '{:.2f}'.format(data.load_avg),
         )
         formatted = templated.rjust(64, " ")
 
-        user_result = format_with_percent(data)
+        user_result = format_with_percent()
         self.assertEqual(user_result, formatted)
 
 
